788ce9e16e36f8207cbf46b8b681a8f8f9a94d81
Raghu Nema <rnema35@gmail.com>
2025-02-01 01:57:15 -0500

    fixing header issue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Header from './components/header/header';
+import CharacterPage from '/Users/raghunema/dndWikiProject/dnd-wiki/src/components/characterPage/characterPage.js'
+import Home from '/Users/raghunema/dndWikiProject/dnd-wiki/src/components/home/home.js';
+import WorldPage from '/Users/raghunema/dndWikiProject/dnd-wiki/src/components/worldPage/worldPage';
+import Timeline from '/Users/raghunema/dndWikiProject/dnd-wiki/src/components/timeline/timeline';
 
 function App() {
   return (
     <div className="App">
-        <Header></Header>
+        <Router>
+          <Header/>
+          <div className="main-content">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/characters" element={<CharacterPage />} />
+                <Route path="/world-page" element={<WorldPage />} />
+                <Route path="/timeline" element={<Timeline />} />
+            </Routes>
+          </div>
+        </Router>
     </div>
   );
 }
