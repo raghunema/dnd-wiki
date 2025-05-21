7391b9884faf4df330ca9251580513ef1c63a009
Raghu Nema <rnema35@gmail.com>
2025-05-20 21:04:37 -0700

    add, update, delete working with recursive cleanup

--- preprocess/diffEngine.js
+++ preprocess/diffEngine.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { type } = require('os');
 const path = require('path');
 
 
@@ -13,52 +14,83 @@ function traverse(obj, path = []) {
     }
 }
 
-function compare_controller(newData, oldData, changeLog) {
-    var changes =  {};
-    changes.version = 1;
+//function to get all the different keys in the data
+function generateKeys(newData, oldData) {
+    const allKeys = Object.keys(newData);
 
-    for (const key in newData) {
-        changes[key] = compare(key, newData, oldData, 1)
-    } 
-
-    console.log(changes)
-    return changes;
+    for (const key in oldData) {
+        if (!allKeys.includes(key)) {
+            allKeys.push(key)
+        }
+    }
+    return allKeys;
 }
 
+function cleanup(changes) {
+    for (const key in changes) {
+        if (typeof changes[key] == 'object' && changes[key] !== null) {
+            cleanup(changes[key]);
+            if (Object.keys(changes[key]).length === 0) {
+                delete changes[key];
+            } 
+        }
+    }
+}
+
+//main compare function
 function compare(key, newData, oldData, direction) {
     var changes = {};
 
-    console.log(`New Keys: ${Object.keys(newData)}`);
-    console.log(`Old Keys: ${Object.keys(oldData)}`);
+    console.log(`Comparing Key: ${key}`)
 
-    console.log(`Updating key: ${key}`);
-    // console.log('New Data:', newData);
-    // console.log('Old Data:', oldData);
-
-    if (!(key in oldData)) {
+    if (!oldData.hasOwnProperty(key)) { //inserts
         changes[`${key}.Add`] = {'data': newData[key]};
-    } else if (key in oldData) {
+    } else if (!newData.hasOwnProperty(key)) { //deletes
+        changes[`${key}.Delete`] = {'data': newData[key]};
+    } else { //updates
         if (newData[key] != oldData[key]) {
 
             if (typeof newData[key] != 'object') {
                 changes[`${key}.Update`] = {'data': newData[key]}
             } else {
                 changes[`${key}.Update`] = {};
-                for (const childKey in newData[key]) {
+                childKeys = generateKeys(newData[key], oldData[key])
+                console.log(childKeys)
+
+                for (const childKey of childKeys) {
+
+                    console.log(`Child Key: ${childKey}`)
                     changes[`${key}.Update`][childKey] = compare(childKey, newData[key],  oldData[key], 1)
-                    //console.log(Object.keys(changes[`${key}.Update`][childKey]).length)
+                    console.log(`Checking length of [${key}.Update][${childKey}]: ${Object.keys(changes[`${key}.Update`][childKey]).length}`)
+
+
                     if (Object.keys(changes[`${key}.Update`][childKey]).length === 0) {
-                        //console.log('deleting')
+                        console.log(`Deleting: [${key}.Update][${childKey}]`)
                         delete changes[`${key}.Update`][childKey]
                     }
                 } 
-            }
-
-            
-
+            }   
         }
     }
 
+    return changes;
+}
+
+//parent function to get all the keys for the differences
+function compare_controller(newData, oldData, changeLog) {
+    var changes =  {};
+    changes.version = 1;
+
+    const allKeys = generateKeys(newData, oldData);
+    console.log(allKeys)
+
+    for (const key of allKeys) {
+        //console.log(`comparing key: ${key}`)
+        changes[key] = compare(key, newData, oldData, 1)
+    } 
+
+    cleanup(changes);
+    //console.log(changes)
     return changes;
 }
 
